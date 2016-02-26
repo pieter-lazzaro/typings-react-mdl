4f1b2b44b068c6861e4c488eb9a6f787b4cef380
Pieter Lazzaro <pieter.lazzaro@gmail.com>
2016-02-26 14:22:26 -0500

    Initial commit

---
+++ lib/Grid.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/main.d.ts" />
+
+import * as React from "react";
+
+export declare const Cell: React.ComponentClass<any>;
+export declare const Grid: React.ComponentClass<any>;
+
+export default Grid;
---
+++ lib/Layout.d.ts
@@ -0,0 +1,14 @@
+/// <reference path="../typings/main.d.ts" />
+
+import * as React from "react";
+
+
+export declare const Layout: React.ComponentClass<any>;
+export declare const Header: React.ComponentClass<any>;
+export declare const HeaderRow: React.ComponentClass<any>;
+export declare const HeaderTabs: React.ComponentClass<any>;
+export declare const Navigation: React.ComponentClass<any>;
+export declare const Drawer: React.ComponentClass<any>;
+export declare const Content: React.ComponentClass<any>;
+export declare const Spacer: React.ComponentClass<any>;
+
